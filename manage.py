403de4301f41f2d4a8e8a95b419250c30a3301ef
Alex Nikonov <alexnikonoff@gmail.com>
2021-03-18 20:08:40 +0300

    Fix manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@
 import os
 import sys
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anfisa4friends.settings')
